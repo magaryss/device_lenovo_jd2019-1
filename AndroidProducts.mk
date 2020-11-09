336e97812da2048f1d47f455a60e45dc72498382
magaryss <71859385+magaryss@users.noreply.github.com>
2020-11-09 08:16:56 -0300

    update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_jd2019.mk
+    $(LOCAL_DIR)/line_jd2019.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_jd2019-user \
-    derp_jd2019-userdebug \
-    derp_jd2019-eng
+    line_jd2019-user \
+    line_jd2019-userdebug \
+    line_jd2019-eng
