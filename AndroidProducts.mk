12301ab40a35d683e39efefde8193b5b3588764d
magaryss <71859385+magaryss@users.noreply.github.com>
2020-11-09 05:28:44 -0300

    update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cesium_jd2019.mk
+    $(LOCAL_DIR)/derp_jd2019.mk
 
 COMMON_LUNCH_CHOICES := \
-    cesium_jd2019-user \
-    cesium_jd2019-userdebug \
-    cesium_jd2019-eng
+    derp_jd2019-user \
+    derp_jd2019-userdebug \
+    derp_jd2019-eng
